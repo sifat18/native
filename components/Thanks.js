fd7da177f962c469d36c811ac04749cfb15389d4
Sifat <ibnsifat@gmail.com>
2021-12-21 04:35:01 +0600

    thank you image

--- components/Thanks.js
+++ components/Thanks.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Image } from 'react-native'
 import { View, Text } from 'react-native'
-
+import pic from './thanks.png'
 export default function Thanks() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', fontSize: 20, fontWeight: 'bold' }}>
+            <Image source={pic}
+                style={{ height: 300, width: 500 }}
+                resizeMode='contain'
+            ></Image>
             <Text style={{ fontSize: 50, fontWeight: 'bold' }}>Thank you</Text>
         </View >
     )
